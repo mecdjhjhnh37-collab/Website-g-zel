2e3c7c51f08160b52c6d7b328878b5011418a4b9
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-24 21:03:36 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,9 @@ const CACHE_NAME = "mecd-tools-v1";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512-17.png"
 ];
 
 self.addEventListener("install", event => {
@@ -13,4 +15,27 @@ self.addEventListener("install", event => {
     })
   );
 
- 
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
